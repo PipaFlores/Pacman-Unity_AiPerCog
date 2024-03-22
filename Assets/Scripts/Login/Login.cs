1906e11a68195c7fbb6021e9eb1cf3b05d5f2d55
Pipa666 <pablo.flores@helsinki.fi>
2024-03-22 16:38:32 +0200

    Implemented user register and login

---
+++ Assets/Scripts/Login/Login.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+using TMPro;
+
+public class Login : MonoBehaviour
+{
+
+    public InputField usernameInput;
+    public InputField passwordInput;
+    public Button loginButton;
+    public Button goToRegisterButton;
+    public string serverUrl = "https://tapir-immune-chimp.ngrok-free.app/";
+
+    public TMP_Text errorMsg;
+    ArrayList credentials;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        loginButton.onClick.AddListener(() => StartCoroutine(LoginUser()));
+        goToRegisterButton.onClick.AddListener(moveToRegister);
+        errorMsg.text = "";
+
+    }
+
+    IEnumerator LoginUser()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("username", usernameInput.text);
+        form.AddField("password", passwordInput.text);
+
+        using (UnityWebRequest www = UnityWebRequest.Post(serverUrl + "login.php", form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(www.error);
+            }
+            else if (www.downloadHandler.text == "Login successful"){
+                // Process the response
+                Debug.Log(www.downloadHandler.text);
+                PlayerPrefs.SetString("username", usernameInput.text);
+                loadWelcomeScreen();
+            }
+            else if (www.downloadHandler.text == "Invalid password"){
+                Debug.Log(www.downloadHandler.text);
+                errorMsg.text = www.downloadHandler.text;
+                yield return new WaitForSeconds(2);
+                errorMsg.text = "";
+            }
+            else if (www.downloadHandler.text == "Username does not exist"){
+                Debug.Log(www.downloadHandler.text);
+                errorMsg.text = www.downloadHandler.text;
+                yield return new WaitForSeconds(2);
+                errorMsg.text = "";
+            }
+            else if (www.downloadHandler.text == "Error opening the file."){
+                Debug.Log(www.downloadHandler.text);
+                errorMsg.text = www.downloadHandler.text;
+                yield return new WaitForSeconds(2);
+                errorMsg.text = "";
+            }
+
+           
+        }
+    }
+
+    void moveToRegister()
+    {
+        SceneManager.LoadScene("Register");
+    }
+
+    void loadWelcomeScreen()
+    {
+        SceneManager.LoadScene("Pacman");
+    }
+}
---
+++ Assets/Scripts/Login/Register.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+using TMPro;
+
+public class Register : MonoBehaviour
+{
+
+    public InputField usernameInput;
+    public InputField passwordInput;
+    public InputField emailinput;
+    public Button registerButton;
+    public Button goToLoginButton;
+    public TMP_Text confirmationText;
+    public string registerUrl = "https://tapir-immune-chimp.ngrok-free.app/";
+    ArrayList credentials;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //confirmationText = GetComponent<TextMeshProUGUI>();
+        registerButton.onClick.AddListener(() => StartCoroutine(RegisterUser()));
+        goToLoginButton.onClick.AddListener(goToLoginScene);
+        confirmationText.text = "";
+        
+    }
+
+    void goToLoginScene()
+    {
+        SceneManager.LoadScene("Login");
+    }
+
+
+IEnumerator RegisterUser()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("username", usernameInput.text);
+        form.AddField("password", passwordInput.text);
+        form.AddField("email", emailinput.text);
+
+        using (UnityWebRequest www = UnityWebRequest.Post(registerUrl + "register.php", form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(www.error);
+                confirmationText.text = "Failed to connect to login server";
+                yield return new WaitForSeconds(2);
+                confirmationText.text = " ";
+                
+            }
+            else
+        {
+            // Check the response from the server
+            if (www.downloadHandler.text == "Username already exists")
+            {
+                // Trigger a special message
+                Debug.Log("Username already exists. Please choose a different username.");
+                confirmationText.text = www.downloadHandler.text;
+                yield return new WaitForSeconds(2);
+                confirmationText.text = "";
+            }
+            else
+            {
+                Debug.Log("User registered successfully.");
+                confirmationText.text = "User registered successfully, go to log in screen";
+                yield return new WaitForSeconds(2);
+                confirmationText.text = "";
+            }
+        }
+        }
+    }
+
+
+}
