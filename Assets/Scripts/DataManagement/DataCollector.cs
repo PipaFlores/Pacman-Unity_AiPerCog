61a52b0a91a36b9eefcadc02751439ffabad6279
Pablo Flores <pablo.flores@helsinki.fi>
2024-05-14 16:49:58 +0300

    Included "localSave" option. Removed csv datatype and methods.

--- Assets/Scripts/DataManagement/DataCollector.cs
+++ Assets/Scripts/DataManagement/DataCollector.cs
@@ -14,9 +14,10 @@ public class DataCollector : MonoBehaviour
     private List<GameDataPoint> dataPointsList = new List<GameDataPoint>();
 
     private string dataserver;
-    public string dataType = "json";
 
-    private int user_id; // FIXME
+    public bool localSave = true; // local save data
+    public bool serverSave = true; // server save data
+    public string localPath = @"C:\LocalData\pabflore\Unity_Projects\unity_pacman\Logs\gamedata.json"; // local path to save data
 
     // Variables to store the game duration
     private double game_duration;
@@ -76,24 +77,7 @@ public class DataCollector : MonoBehaviour
             Debug.Log("Game data uploaded successfully.");
             this.dataPointsList.Clear();
         }
-        }
-        else if (dataType == "csv"){
-            string url = dataserver + "savegamedata_csv.php";
-            // Use a form to send CSV data
-            WWWForm form = new WWWForm();
-            form.AddField("data", gameData);
-
-            UnityWebRequest www = UnityWebRequest.Post(url, form);
-            yield return www.SendWebRequest();
-
-            if (www.result != UnityWebRequest.Result.Success){
-                Debug.Log(www.error);
-                }
-            else{
-                Debug.Log("Game data uploaded successfully.");
-                this.dataPointsList.Clear();
-                }
-        }
+    
         
     }
 
@@ -102,7 +86,6 @@ public class DataCollector : MonoBehaviour
         game_endTime = System.DateTime.UtcNow;
         game_duration = (game_endTime - game_startTime).TotalSeconds;
         
-        if (dataType == "json"){
         CancelInvoke();
         GameDataContainer container = new GameDataContainer 
         { 
@@ -115,34 +98,21 @@ public class DataCollector : MonoBehaviour
             source = MainManager.Instance.source            
         };
         string json = JsonUtility.ToJson(container, true);
+        if (localSave){
+            LocalSaveData(json);
+        }
+        if (serverSave){
             StartCoroutine(SendGameData(json));
         }
-        else if (dataType == "csv"){
-            CancelInvoke();
-            List<string> csvLines = new List<string>(); // headers initialized in server's php
-            foreach (var dataPoint in dataPointsList){
-                csvLines.Add(dataPoint.ToCsvString()); // Implement ToCsvString in GameDataPoint
-            }
-            
-            string csvString = string.Join("\n", csvLines);
-            StartCoroutine(SendGameData(csvString));
-        }
-        else{
-            Debug.Log("Select a valid data type (json or csv)");
-        }        
+          
     }
-    public void LocalSaveData()  // Local save data
+    public void LocalSaveData(string json)  // Local save data in json format
     {
-        GameDataContainer container = new GameDataContainer { dataPoints = dataPointsList };
-        string json = JsonUtility.ToJson(container, true);
-
-        // Logic to save json to a file or send it to a server
-        string fileName = @"C:\LocalData\pabflore\Unity_Projects\unity_pacman\Logs\gamedata.json";
-        string directory = Path.GetDirectoryName(fileName);
+        string directory = Path.GetDirectoryName(localPath);
         if (!Directory.Exists(directory)){
             Directory.CreateDirectory(directory);
         }
-        System.IO.File.WriteAllText(fileName, json);
+        System.IO.File.WriteAllText(localPath, json);
         //@"D:\PacmanUnity\Logs\Datacollection\data.json"
         // "../Datacollection2/data.json" writes to d:\Datacoll....
         //"file:../Datacollection2/data.json" gets the local path but adds the "file:.."
