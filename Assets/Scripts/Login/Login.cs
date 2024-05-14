e5200be7a2db08ca76f8f7605379dc4cac9cf52b
Pablo Flores <pablo.flores@helsinki.fi>
2024-05-14 16:52:54 +0300

    Fixed frightened ghosting animated flashing

--- Assets/Scripts/Login/Login.cs
+++ Assets/Scripts/Login/Login.cs
@@ -18,7 +18,7 @@ public class Login : MonoBehaviour
     private string serverUrl;
 
     public TMP_Text errorMsg;
-    ArrayList credentials;
+
 
     // Start is called before the first frame update
     void Start()
